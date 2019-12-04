59ff7848bc6d5359bb32660ab996c23dcba34a98
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-12-04 18:02:07 +0200

    Browser mode is now working

--- plugins/system/darkmagic/media/js/darkmagic.js
+++ plugins/system/darkmagic/media/js/darkmagic.js
@@ -40,36 +40,126 @@ nikosdion.darkMagic.listener = function (e) {
     var cssId       = "nikosdionDarkMagic";
     var darkModeCSS = "../plugins/system/darkmagic/darktheme/administrator/templates/isis/css/custom.css";
 
+    // Remove the TinyMCE skin CSS
+    nikosdion.darkMagic.removeCSSFile("lightgray/skin.min.css");
+    nikosdion.darkMagic.removeCSSFile("lightgray/skin.css");
+    nikosdion.darkMagic.removeCSSFile("charcoal/skin.min.css");
+    nikosdion.darkMagic.removeCSSFile("charcoal/skin.css");
+
     // Dark Mode
     if (e.matches)
     {
-        // Already dark?
-        if (document.getElementById(cssId))
+        // Add the Dark Mode CSS
+        nikosdion.darkMagic.injectCSSFile(cssId, darkModeCSS)
+
+        // Load the Charcoal TinyMCE sking
+        nikosdion.darkMagic.injectCSSFile("darkMagicTinyMCETemplate",
+            "../media/editors/tinymce/skins/charcoal/skin.min.css");
+        nikosdion.darkMagic.reskinTinyMCE('charcoal');
+
+        return;
+    }
+
+    //  Remove the Dark Mode CSS
+    nikosdion.darkMagic.removeCSSFile(darkModeCSS)
+
+    // Load the LightGray TinyMCE sking
+    nikosdion.darkMagic.injectCSSFile("darkMagicTinyMCETemplate",
+        "../media/editors/tinymce/skins/lightgray/skin.min.css");
+    nikosdion.darkMagic.reskinTinyMCE('lightgray');
+};
+
+nikosdion.darkMagic.reskinTinyMCE = function (skinName) {
+    if (typeof window.tinyMCE === "undefined")
+    {
+        return;
+    }
+
+    // TODO Check if I should mess with TinyMCE (both option and whether tinymce exists)
+    if (!true)
+    {
+        return;
+    }
+
+    // Set the skin
+    Joomla.optionsStorage.plg_editor_tinymce.tinyMCE.default.skin = skinName;
+
+    // Commit and remove all TinyMCE editors
+    var deleteKeys = [];
+
+    for (var key in Joomla.editors.instances)
+    {
+        if (!Joomla.editors.instances.hasOwnProperty(key))
         {
-            return;
+            continue;
         }
 
-        var head   = document.getElementsByTagName("head")[0];
-        var link   = document.createElement("link");
-        link.id    = cssId;
-        link.rel   = "stylesheet";
-        link.type  = "text/css";
-        link.href  = darkModeCSS;
-        link.media = "screen";
-        head.appendChild(link);
+        // Commit the content
+        var thisEditor = Joomla.editors.instances[key];
 
-        return;
+        // Make sure it's TinyMCE and remove it
+        var thisInstance = thisEditor.instance;
+
+        if (
+            (typeof thisInstance.plugins !== "object") ||
+            (typeof thisInstance.remove !== "function") ||
+            (typeof thisEditor.onSave !== "function")
+        )
+        {
+            // Probably not TinyMCE...
+            continue;
+        }
+
+        var textboxID = thisEditor.id;
+
+        thisInstance.loadedCSS = {};
+        thisInstance.remove();
+        deleteKeys.push(key);
     }
 
-    // Light Mode
-    if (!document.getElementById(cssId))
+    // If I didn't remove at least one editor I'm done.
+    if (deleteKeys.length === 0)
     {
-        // Already Light Mode
         return;
     }
 
-    // Get all link elements
+    // Delete Joomla's internal editor keys
+    deleteKeys.map(function (key) {
+        delete Joomla.editors.instances[key];
+    });
+
+    // Re-render TinyMCE editors, document and subforms
+    Joomla.JoomlaTinyMCE.setupEditors();
+
+    if (window.jQuery)
+    {
+        jQuery(document).on("subform-row-add", function (event, row) {
+            Joomla.JoomlaTinyMCE.setupEditors(row);
+        });
+    }
+};
+
+nikosdion.darkMagic.injectCSSFile = function (cssId, darkModeCSS) {
+    if (document.getElementById(cssId))
+    {
+        return false;
+    }
+
+    var head   = document.getElementsByTagName("head")[0];
+    var link   = document.createElement("link");
+    link.id    = cssId;
+    link.rel   = "stylesheet";
+    link.type  = "text/css";
+    link.href  = darkModeCSS;
+    link.media = "screen";
+    head.appendChild(link);
+
+    return true;
+};
+
+nikosdion.darkMagic.removeCSSFile = function (partialURL) {
     var allLinks = document.getElementsByTagName("link");
+    var result   = false;
 
     // Find the element with our CSS file and send it to oblivion
     for (var i = 0; i < allLinks.length; i++)
@@ -86,13 +176,16 @@ nikosdion.darkMagic.listener = function (e) {
             continue;
         }
 
-        if (href !== darkModeCSS)
+        if (href.substr(-partialURL.length) !== partialURL)
         {
             continue;
         }
 
         allLinks[i].parentNode.removeChild(allLinks[i]);
+        result = true;
     }
+
+    return result;
 };
 
-nikosdion.darkMagic.initialize();
+setTimeout(nikosdion.darkMagic.initialize, 250);
