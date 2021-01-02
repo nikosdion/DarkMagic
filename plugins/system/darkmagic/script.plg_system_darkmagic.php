efba34bfbb403ba245a6a40484854b294eaecf80
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-02 16:50:03 +0200

    Updating copyright. Happy New Year!

--- plugins/system/darkmagic/script.plg_system_darkmagic.php
+++ plugins/system/darkmagic/script.plg_system_darkmagic.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *  @package   DarkMagic
- *  @copyright Copyright (c)2019-2020 Nicholas K. Dionysopoulos
+ *  @copyright Copyright (c)2019-2021 Nicholas K. Dionysopoulos
  *  @license   GNU General Public License version 3, or later
  */
 
